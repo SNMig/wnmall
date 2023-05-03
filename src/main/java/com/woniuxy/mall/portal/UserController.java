2af70aa30ac9fa4ae05656fee8fe0186a424e4be
SNMig <106984872+SNMig@users.noreply.github.com>
2023-05-03 21:29:34 +0800

    1.5

--- src/main/java/com/woniuxy/mall/portal/UserController.java
+++ src/main/java/com/woniuxy/mall/portal/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
 @Controller
 @Slf4j
 public class UserController {
@@ -16,6 +18,8 @@ public class UserController {
     private JavaMailSenderImpl javaMailSender;
     @Autowired
     private UserService userService;
+    @Autowired
+    private RestTemplate restTemplate;
     @GetMapping("/register")
     public String register() {
         return "register";
@@ -30,15 +34,30 @@ public class UserController {
     @PostMapping("/register")
     public String register(User user){
         //yepzkejtozjlbfee
-        //log.debug("注册用户信息{}",user);
-        //userService.save(user);
-        SimpleMailMessage message = new SimpleMailMessage();
+        log.debug("注册用户信息{}",user);
+        userService.save(user);
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep(1000); //模拟1秒的操作时长
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-        message.setFrom("1048321255@qq.com");
-        message.setTo(user.getEmail());
-        message.setSubject("激活");
-        message.setText("激活连接");
-        javaMailSender.send(message);
+        new Thread(() -> {
+            String result = restTemplate.getForObject("http://192.168.11.191:8081/api/sendmail?id=2&email=" + user.getEmail(), String.class);
+            System.out.println(result);
+        }).start();
+        long end = System.currentTimeMillis();
+        System.out.println("时长：" + (end - start));
+
+
+//        SimpleMailMessage message = new SimpleMailMessage();
+//
+//        message.setFrom("1048321255@qq.com");
+//        message.setTo(user.getEmail());
+//        message.setSubject("激活");
+//        message.setText("激活连接");
+//        javaMailSender.send(message);
         return "login";
     }
 }
